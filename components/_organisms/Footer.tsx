aa102e3e2f4e8e4f4923b687a86571763ba2a3de
avinash7rao <avinash7rao@gmail.com>
2026-06-24 15:11:27 -0400

    footer updated

--- components/_organisms/Footer.tsx
+++ components/_organisms/Footer.tsx
@@ -1,9 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import { CopyRight } from "../_atoms/Icons/CopyRight";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import IconButton from "@mui/material/IconButton";
 interface FooterData {
   items: {
     field_company_name: string;
     field_year: number;
+    youtube_link: string;
   };
 }
 interface FooterProps {
@@ -12,24 +15,38 @@ interface FooterProps {
 
 export function Footer({ footer }: FooterProps) {
   return (
-    <Box
-      component='footer'
-      sx={{
-        mb: "auto",
-        py: 2,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 0.5,
-      }}>
-      <CopyRight />
-      <Typography
-        variant='body2'
+    <Box component='footer' sx={{ py: 2 }}>
+      <Box
         sx={{
-          color: "common.white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 1,
         }}>
-        {footer?.items?.field_company_name} {footer?.items?.field_year}
-      </Typography>
+        <IconButton
+          href={footer?.items?.youtube_link}
+          target='_blank'
+          rel='noopener noreferrer'>
+          <YouTubeIcon sx={{ color: "common.white", fontSize: 40 }} />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          mb: "auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 0.5,
+        }}>
+        <CopyRight />
+        <Typography
+          variant='body2'
+          sx={{
+            color: "common.white",
+          }}>
+          {footer?.items?.field_company_name} {footer?.items?.field_year}
+        </Typography>
+      </Box>
     </Box>
   );
 }
--- data/footerData.tsx
+++ data/footerData.tsx
@@ -2,6 +2,7 @@ const footerData = {
   items: {
     field_company_name: "Renshu Media Production & Technologies LLC",
     field_year: 2026,
+    youtube_link: "https://www.youtube.com/channel/UC-4tH4CCBTqdSuB3oqJnZNg",
   },
 };
 
